55e15d012c6c0ec12edf9b7fa9e50f6a0e3cb386
rayferric <63957587+rayferric@users.noreply.github.com>
2024-10-03 19:35:51 +0200

    remove apt update before cyclonedds installation + update apt in distrobox bashrc if needed

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -6,7 +6,6 @@ export _KALMAN_WS_ROOT=$(realpath $(dirname $BASH_SOURCE)/..)
 # Use this: /opt/ros/humble/lib/librmw_cyclonedds_cpp.so
 if [ ! -f "/opt/ros/humble/lib/librmw_cyclonedds_cpp.so" ]; then
     echo "Cyclone DDS is not installed. Installing..."
-    sudo apt-get update
     sudo apt-get install -y ros-humble-rmw-cyclonedds-cpp
 fi
 # Enable Cyclone DDS.
