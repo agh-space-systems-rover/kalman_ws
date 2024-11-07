51d88a79c6983be7138098b072fc9ad041dabd8a
rayferric <63957587+rayferric@users.noreply.github.com>
2024-11-07 22:25:16 +0100

    suppress cmake warning "Manually-specified variables were not used by the project""

--- scripts/macros.bash
+++ scripts/macros.bash
@@ -111,7 +111,7 @@ build() {
 
     # Build the workspace.
     echo "Building packages..."
-    colcon build --symlink-install --base-paths src --packages-select $pkg_names --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+    colcon build --symlink-install --base-paths src --packages-select $pkg_names --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --no-warn-unused-cli
     if [ $? -ne 0 ]; then
         echo "Failed to build some packages."
         cd $prev_dir
